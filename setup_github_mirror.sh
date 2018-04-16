74d863cb7fdbf5eb825478eccf9fe3f3040494e1
Ian Service <ian@service.to>
2018-04-16 13:09:56 -0400

    remove script attempt

--- setup_github_mirror.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if ! grep -q github.com ~/.ssh/known_hosts ; then
-	ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
-fi
-
-if ! grep -q "\[remote \"github\"\]" .git/config ; then
-	git remote add github $1
-fi
