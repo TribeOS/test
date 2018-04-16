5f5e0aecaaab665a1761c949577cc6f889bcb000
Ian Service <ian@service.to>
2018-04-16 12:29:58 -0400

    test a more specific line to heal the runner if broked

--- setup_github_mirror.sh
+++ setup_github_mirror.sh
@@ -4,6 +4,6 @@ if ! grep -q github.com ~/.ssh/known_hosts ; then
 	ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 fi
 
-if ! grep -q github .git/config ; then
+if ! grep -q "\[remote \"github\"\]" .git/config ; then
 	git remote add github $1
 fi
