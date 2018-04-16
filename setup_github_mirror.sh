8df74cc46218783716d7f8d24a1029223b7fdbd2
Ian Service <ian@service.to>
2018-04-16 12:14:29 -0400

    move the github logic to a bash script

---
+++ setup_github_mirror.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if ! grep -q github.com ~/.ssh/known_hosts ; then
+	ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+fi
+
+if ! grep -q github .git/config ; then
+	git remote add github $1
+fi
